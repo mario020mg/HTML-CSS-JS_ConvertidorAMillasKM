c6b2af8bb0d8defc15a5b41b63486c8e1a078579
Mario Manzanares Galvany <58348457+mario020mg@users.noreply.github.com>
2025-02-13 16:43:55 +0100

    V1

---
+++ script.js
@@ -0,0 +1,17 @@
+function convert(){
+    let inputValue = 0;
+    let result = 0;
+
+    inputValue = document.getElementById("userInput").value;
+    let unit = document.getElementById("unit").value;
+    if (unit === "milesToKm") {
+        result = 1.60934 * inputValue;
+    } else {
+        result = inputValue / 1.60934;
+    }
+    const resultString = inputValue + " miles are " + result + " km";
+    console.log(resultString);
+
+    const resultElement = document.getElementById('resultElement');
+    resultElement.innerHTML = resultString;
+}
